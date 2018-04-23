b43a63471c00f13069d4db05ef24084b9580194d
lsy <lsy@lsydeMacBook-Pro.local>
2018-04-23 14:47:47 +0800

    新增部分前端页面
    备份

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,26 @@
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    currentTab: 0,
+    items: [
+      { name: '1', value: '延迟安装', checked: 'true' },
+      { name: '2', value: '取消安装', },
+    ],
+    reasons: [
+      { name: '1', value: '和用户协商推迟', checked: 'true' },
+      { name: '2', value: '用户要求推迟'},
+      { name: '3', value: '用户要求取消'},
+    ]
   },
+  show: "",  
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  
 
   /**
    * 生命周期函数--监听页面加载
@@ -61,5 +76,68 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+
+  //滑动切换
+  swiperTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  scanclick: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = "结果:" + res.result + "二维码类型:" + res.scanType + "字符集:" + res.charSet + "路径:" + res.path;
+        that.setData({
+          show: this.show
+        })
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: (res) => {
+      }
+    })
+  },
+  subCode: function () {
+    //保留当前页面，跳转到应用内的某个页面，使用wx.navigateBack可以返回到原页面。
+    wx.navigateTo({
+      url: '../index/getPhoto',
+      success: function (res) {
+        // success
+        console.log(成功);
+      },
+      fail: function () {
+        // fail
+        console.log('sksdfksjfksjf');
+      },
+      complete: function () {
+        // complete
+      }
+    })
   }
 })
+
