5d00765f15967a94a997921bffc1d693c0c777bc
lsy <lsy@lsydeMacBook-Pro.local>
2018-05-04 21:43:25 +0800

    部分修改

--- pages/phone/phone.js
+++ pages/phone/phone.js
@@ -24,15 +24,16 @@ Page({
   phonebind: function (e) {
     var that = this
     console.log("openid"+that.openid);
+    console.log("memberphone" + e.detail.value.bindphone);
     wx.request({
-      url: 'https://microservice.gmair.net//installation/member/setwechat',
+      url: 'https://microservice.gmair.net/auth/install/bind',
       header: {
         "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
       },
       method: "POST",
       data: { memberPhone: e.detail.value.bindphone, wechatId:that.openid },
       success: function (res) {
-        if (res.responseCode.equals("RESPONSE_OK")){
+        if (res.responseCode==("RESPONSE_OK")){
           wx.redirectTo({
             url: '/pages/index/index'
           })
